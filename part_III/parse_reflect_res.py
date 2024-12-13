4ad9d084bca687835d3f2ad6bff3e7c38a429c9c
mushengL <159851757+mushengL@users.noreply.github.com>
2024-12-12 22:42:42 -0800

    Update parse_reflect_res.py
    Add comments.

--- part_III/parse_reflect_res.py
+++ part_III/parse_reflect_res.py
@@ -1,11 +1,13 @@
 import json
 import os
 
-directory = "/Users/vctrlin/data/stanford_online/winter_quater/CS224W/final_project/GNN_RAG/cs224w"
+# set working directory
+directory = "path to working directory"
 
+# get the test results
 res = os.path.join(directory, "_test_reflect.info")
 
-
+# parse the test result from json format per line 
 tmp_data = {}
 cnt = 0
 tot = 0
@@ -13,9 +15,10 @@ with open(res, "r") as f:
     for line in f:
         tot += 1
         tmp = json.loads(line)
+
+        # count the case if answer is among candidates, "exact match" == 1
         if tmp["em"] == 1:
             cnt += 1
 
-
-
+# calculate the improvement percentage
 print("improve rate: " + str(round((cnt / tot) * 100, 2)))
